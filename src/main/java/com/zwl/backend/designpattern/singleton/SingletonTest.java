8438760f169eed2a8c28ca761c74952f4698354e
zwl <1448470255@qq.com>
2021-07-20 15:13:03 +0800

    ..

---
+++ src/main/java/com/zwl/backend/designpattern/singleton/SingletonTest.java
@@ -0,0 +1,16 @@
+package com.zwl.backend.designpattern.singleton;
+
+import org.junit.Test;
+
+/**
+ * @author zwl
+ * @date 2021/7/20 15:01
+ * @describe instructions...
+ */
+public class SingletonTest {
+
+    @Test
+    public void test01(){
+
+    }
+}
